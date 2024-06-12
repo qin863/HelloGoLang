549c3cb40c6355ac570b9561874ebf64f665058d
Amari Turner <624208829@qq.com>
2024-06-12 23:13:50 +0800

    go语言语法 初始化

---
+++ 19.switch/switch.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+)
+
+func main() {
+	fmt.Print("Go 运行的系统环境：")
+	switch os := runtime.GOOS; os {
+	case "darwin":
+		fmt.Println("macOS.")
+	case "linux":
+		fmt.Println("linux")
+	default:
+		fmt.Printf("%s.\n", os)
+	}
+}
